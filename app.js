e4581bd6f92a1a3bd0d6849dc9162bebaae967c8
Christian Aromin <c.aromin.unf@gmail.com>
2018-08-27 17:16:46 -0400

    finished basic layout

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon')
+var path = require('path')
 
 var indexRouter = require('./routes/index');
 
@@ -16,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
