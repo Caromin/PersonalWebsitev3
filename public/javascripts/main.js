eb63176f67cd8e8199e42e937a9f82abbbc4a2d1
Christian Aromin <c.aromin.unf@gmail.com>
2018-08-28 04:12:13 -0400

    animations and scrollreveal added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    console.log('meow');
+    const slideUp = {
+        distance: '150%',
+        origin: 'bottom',
+        opacity: 0,
+        delay: 0,
+        duration: 2000,
+        interval: 1000,
+        viewOffset: {
+            bottom: 100
+        }
+    };
+
+    const slideRight = {
+        distance: '150%',
+        origin: 'right',
+        opacity: 1,
+        delay: 0,
+        duration: 2000,
+        interval: 500,
+    }
+
+    ScrollReveal().reveal('.slide-right', slideRight);
+    ScrollReveal().reveal('.slide-up', slideUp);
 });
