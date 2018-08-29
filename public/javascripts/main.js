7e28ae32acff7b8bf788c883b9b35f75298c497f
Christian Aromin <c.aromin.unf@gmail.com>
2018-08-28 20:07:12 -0400

    converted some html into dynamic ajax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,6 +20,80 @@ $(document).ready(function(){
         interval: 500,
     }
 
+    // generates the progress bars
+    $.ajax({
+        url: '../stylesheets/skillset.json',
+        datatType: 'json',
+        type: 'get',
+        cache: false,
+        success: function(data){
+            // first point is which part of the array you are in
+            // second param is the data, renamed as anything
+            $(data.skills).each(function(index, info){
+                let title = info.title;
+                let style = info.style;
+                let value = info.value;
+                let skillProgressBar = 
+                    `<div class='marginbottom marginright widthProgressBar slide-up'>
+                        <p class='bluetextbold text-uppercase'>${title}</p>
+                        <div class='progressBarContainer'>
+                            <div class="progress">
+                            <div class="progress-bar-animated progress-bar progress-bar-striped" role="progressbar" style="width: ${style}" aria-valuenow="${value}" aria-valuemin="0" aria-valuemax="100">
+                            </div>
+                            </div>
+                        </div>
+                    </div>`
+
+                $('#skillset').append(skillProgressBar);
+                ScrollReveal().reveal('.slide-up', slideUp);
+            })
+        }
+    });
+
+    // generates the projects section
+    $.ajax({
+        url: '../stylesheets/languages.json',
+        datatType: 'json',
+        type: 'get',
+        cache: false,
+        success: function(data){
+            // first point is which part of the array you are in
+            // second param is the data, renamed as anything
+            $(data.projects).each(function(index, info){
+                let active = info.active;
+                let id = info.id;
+                let link = info.link;
+                let title = info.title;
+                let languages = info.languages;
+                var newLanguage = "";
+                
+                $.each(languages, function(index, value){
+                    let language = `
+                    <div class='d-flex align-items-center blacktextborder justify-content-center'>
+                        ${value}
+                    </div>`;
+
+                    newLanguage += language;
+                })
+
+                let projectContainer = `
+                    <div class='marginbottom d-flex align-items-start flex-column'>
+                        <div id='${id}' class='projects'>
+                            <a class='fullcover' href='${link}' target="_blank"></a>
+                        </div>
+                        <div class='marginbottom margintop'>
+                            <a class='blacktextbold' href='${link}' target="_blank">${title}</a>
+                        </div>
+                        <div class='width75p d-flex flex-wrap'>
+                            ${newLanguage}
+                        </div>
+                    </div>`;
+
+                $('#finishedProjects').append(projectContainer);
+                
+            })
+        }
+    });
+
     ScrollReveal().reveal('.slide-right', slideRight);
-    ScrollReveal().reveal('.slide-up', slideUp);
 });
